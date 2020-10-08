cc194e08accff027ed8c48494d40c9bfd6a31147
MARTÍN ROMERAL ALONSO <Martin.Romeral@alu.uclm.es>
2020-10-08 19:23:28 +0200

    nuevas funciones 7/10/20

--- cliente/modelo.js
+++ cliente/modelo.js
@@ -1,42 +1,74 @@
-function Juego(){
-    this.partidas = {};//! Que colección usamos?
-    this.crearPartida=function(num,owner) {
-        //* Generar código de 6 letras 
-        //* Crear el objeto partida: num owner
-        //! this.partidas[codigo] = nueva partida
-        let codigo = this.obtenerCodigo();
-        if(!this.partidas[codigo]){
-            this.partidas[codigo] = new Partida(num, owner);
-        }
+function Juego() {
+  this.partidas = {}; //! Que colección usamos?
+  this.crearPartida = function (num, owner) {
+    let codigo = this.obtenerCodigo();
+    if (!this.partidas[codigo]) {
+      this.partidas[codigo] = new Partida(num, owner);
     }
+  };
 
-    this.unirAPartida=function(nick){
-
+  this.unirAPartida = function (codigo, nick) {
+    if (this.partidas[codigo] && this.faltan(codigo)) {
+      this.partidas[codigo].agregarUsuario(nick);
     }
+  };
 
-    this.obtenerCodigo=function(){
-        let cadena = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        let letras = cadena.split('');
-        let codigo = [];
-        for(i = 0; i<6;i++){
-            codigo.push(letras[randomInt(0,25)-1]);
-        }
-        return codigo.join('');
+  this.faltan = function (codigo) {
+    return Object.keys(juego.partidas[codigo].usuarios).length;
+  };
+
+  this.obtenerCodigo = function () {
+    let cadena = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let letras = cadena.split("");
+    let maxCadena = cadena.length;
+    let codigo = [];
+    for (i = 0; i < 6; i++) {
+      codigo.push(letras[randomInt(1, maxCadena) - 1]);
+    }
+    return codigo.join("");
+  };
+}
+
+function Partida(num, owner) {
+  this.max = num;
+  this.nickOwner = owner;
+  this.usuarios = []; //* El index 0 será el owner
+  //* this.usuarios = {}; //! Versión array asociativo o diccionario
+  this.agregarUsuario=function(nick){
+      this.fase.agregarUsuario(nick);
+  }
+  this.agregarUsuario = function (nick) {
+    let nuevo = nick;
+    let contador = 1;
+    while (this.usuarios[nuevo]) {
+      nuevo = nick + contador;
+      contador = contador + 1;
+    }
+    this.usuarios[nuevo] = new Usuario(nuevo);
+  };
+  this.agregarUsuario(owner);
+}
+
+function Inicial() {
+    this.agregarUsuario=function(nick,partida){
+        partida.puedeAgregarUsuario(nick);
+    }
+}
+function Jugando() {
+    this.agregarUsuario=function(nick,partida){
+        //this.puedeAgregar(nick);
+    }
+}
+function Final() {
+    this.agregarUsuario=function(nick,partida){
+        //this.puedeAgregar(nick);
     }
 }
 
-function Partida(num,owner){
-    this.numUsuarios = num;
-    this.owner = owner;
-    this.usuarios = [];//* El index 0 será el owner
-    //* this.usuarios = {}; //! Versión array asociativo o diccionario
-    this.agregarUsuario=function(nick){
-        //Comprobar nick único
-        //Comprobar si es el usuario num(máximo)
-    }
-    this.agregarUsuario(owner);
+function Usuario(nick) {
+  this.nick = nick;
 }
 
 function randomInt(low, high) {
-	return Math.floor(Math.random() * (high - low) + low);
+  return Math.floor(Math.random() * (high - low) + low);
 }
